35884fafc46916788c3c083f024507997ac1f223
shobhit157 <you@example.com>
2026-03-15 23:32:54 +0530

    added frontend url placeholders

--- src/main/java/com/example/Backend_dev_assgn/security/SecurityConfig.java
+++ src/main/java/com/example/Backend_dev_assgn/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.Backend_dev_assgn.security;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 	
 	private final JwtFilter jwtFilter;
+	
+	@Value("${frontend.url}")
+    private String frontendUrl;
 
     public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
@@ -30,7 +34,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .cors(cors -> cors.configurationSource(request -> {
                 var corsConfig = new org.springframework.web.cors.CorsConfiguration();
-                corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+                corsConfig.setAllowedOrigins(List.of(frontendUrl));
                 corsConfig.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
                 corsConfig.setAllowedHeaders(List.of("*"));
                 corsConfig.setAllowCredentials(true);
