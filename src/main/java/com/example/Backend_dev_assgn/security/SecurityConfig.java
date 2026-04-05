4b67b77691e157928d281cf9ee4d110e169a6a4b
shobhit157 <you@example.com>
2026-04-06 00:35:30 +0530

    chnges made to security and config

--- src/main/java/com/example/Backend_dev_assgn/security/SecurityConfig.java
+++ src/main/java/com/example/Backend_dev_assgn/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.Backend_dev_assgn.security;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
